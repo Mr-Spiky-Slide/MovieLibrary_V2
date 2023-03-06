66e9435e74cd388928bd1f6ec5ba03b3576051fa
JackHoward <jhoward000@live.com>
2023-03-06 13:11:20 -0600

    store movie properties

--- Program.cs
+++ Program.cs
@@ -5,14 +5,23 @@
 
  // create instance of Logger
  var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
+string movieFilePath = Directory.GetCurrentDirectory() + "//movies.csv";
+
  logger.Info("Program started");
 
-Movie movie = new Movie
+ MovieFile movieFile = new MovieFile(movieFilePath);
+
+string choice = "";
+ do
  {
-   movieId = 1,
-   title = "Jeff's Killer Movie (2019)",
-   genres = new List<string> { "Action", "Romance", "Comedy" }
- };
+   // display choices to user
+   Console.WriteLine("1) Add Movie");
+   Console.WriteLine("2) Display All Movies");
+   Console.WriteLine("Enter to quit");
+   // input selection
+   choice = Console.ReadLine();
+   logger.Info("User choice: {Choice}", choice);
+ } while (choice == "1" || choice == "2");
+
  logger.Info("Program ended");
 
-Console.WriteLine(movie.Display());
